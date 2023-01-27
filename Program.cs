681843e3039449cdd032d53ce4de67c84dd693ab
Ashwini <salunkheashwini1998@gmail.com>
2023-01-27 21:19:06 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Indian_States_Census_Analyzer_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ***** Welcome to Indian States Census Analyser program *****");
+
+        }
+    }
+}
