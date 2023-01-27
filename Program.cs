1d8d42969e452f392d19a48aa8eafcce2127ce9d
Ashwini <salunkheashwini1998@gmail.com>
2023-01-27 23:38:23 +0530

    method calling

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day29 Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ***** Welcome to Indian States Census Analyser program *****");
+
+        }
+    }
+}
