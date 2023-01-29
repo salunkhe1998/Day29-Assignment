eaa970095fca04dcd0549dd7311c0cdc2b4ba032
Ashwini <salunkheashwini1998@gmail.com>
2023-01-29 11:14:59 +0530

    censsusAnalyzer....

---
+++ CensusAnalyzer.cs
@@ -0,0 +1,26 @@
+﻿using Indian_States_Census_Analyzer_Problem.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indian_States_Census_Analyzer_Problem
+{
+    public class CensusAnalyzer
+    {
+        public enum Country
+        {
+            INDIA
+        }
+
+        Dictionary<string, CensusDTO> dataMap;
+
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath, string dataHeaders)
+        {
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+
+    }
+}
