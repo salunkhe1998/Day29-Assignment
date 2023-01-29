d03fcf018df9010a581bf226bc76bb758362b868
Ashwini <salunkheashwini1998@gmail.com>
2023-01-29 11:52:44 +0530

    Analyzer Exception...

---
+++ CensusAnalyzerException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indian_States_Census_Analyzer_Problem
+{
+    public class CensusAnalyserException : Exception
+    {
+        public enum ExceptionType
+        {
+            FILE_NOT_FOUND, INVALID_FILE_TYPE, INCORRECT_DELIMETER, INCORRECR_HEADER, NO_SUCH_COUNTRY
+        }
+
+        public ExceptionType exType;
+
+        public CensusAnalyserException(string message, ExceptionType exceptionType) : base(message)
+        {
+            this.exType = exceptionType;
+        }
+
+    }
+
+}
